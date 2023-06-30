2eb0043220f3d4b85456029dc10f7fa97c703c8c
franciscohenriquede <104444467+franciscohenriquede@users.noreply.github.com>
2023-06-30 16:33:49 -0300

    auta
    subindo

---
+++ filmeportal/recursividade/recursividade.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String args[]) {
+   
+    int resultado=0;  
+   resultado = soma(5, 6 );
+   System.out.print(resultado);
+
+
+       
+}
+   static int  soma(int n , int m){
+     
+     return n+m;
+   }
+
